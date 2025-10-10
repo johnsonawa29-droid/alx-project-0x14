c2fd14ad946568c8e48dc4311074fa0dfcfd26bc
johnson <johnsonawa29@gmail.com>
2025-10-10 16:43:30 +0100

    yes

---
+++ alx-movie-app/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white text-center py-3 mt-8">
+      <p>© 2025 ALX Movie App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
