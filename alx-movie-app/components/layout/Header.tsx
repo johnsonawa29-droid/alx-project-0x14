2ca6c62e3458b3d5c88824774beb8622964ff9f0
johnson <johnsonawa29@gmail.com>
2025-10-10 16:42:22 +0100

    yes

---
+++ alx-movie-app/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-900 text-white p-4">
+      <h1 className="text-xl font-bold">🎬 ALX Movie App</h1>
+    </header>
+  );
+};
+
+export default Header;
